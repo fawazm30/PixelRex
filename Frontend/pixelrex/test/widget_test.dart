40702cc3429132cc07236de3231c2c8f4a95a23a
Fawaz <fawazarshad30@gmail.com>
2025-02-20 11:20:01 -0700

    oh yeah baby its all coming together

--- Frontend/pixelrex/test/widget_test.dart
+++ Frontend/pixelrex/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pixelrecs/main.dart';
+import 'package:PixelRex/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
